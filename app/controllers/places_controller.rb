beeb7abb5862372452ddced8e56b619dd5c79e5e
jem008 <joe.mcnulty@kellogg.northwestern.edu>
2022-02-21 22:03:08 +0000

    committing and syncing regularly

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,18 @@ class PlacesController < ApplicationController
         @places = Place.all
     end
 
+    def show
+        @place = Place.find(params["id"])
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+
 end
