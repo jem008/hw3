5792c9a7030d30e1a8919131dc48a8289ed9d268
jem008 <joe.mcnulty@kellogg.northwestern.edu>
2022-02-21 23:14:31 +0000

    committing and syncing regularly

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,18 @@ class PostsController < ApplicationController
         @posts = Post.all
     end
 
+    def show
+        @post = Post.find(params["id"])
+    end
+
     def new
         @post = Post.new
+        @post.place_id = params["place_id"]
     end
 
     def create
-        @post = Post.new(params["post"])
-        @post.save
-        redirect_to "/places"
+       @post = Post.new(params["post"])
+       @post.save
+       redirect_to "/places"
     end
 end
