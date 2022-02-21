002e14d92d171584679d49b56443810960c4f06f
jem008 <joe.mcnulty@kellogg.northwestern.edu>
2022-02-21 23:17:32 +0000

    committing and syncing regularly

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,5 @@
 class PostsController < ApplicationController
 
-    def index
-        @posts = Post.all
-    end
-
-    def show
-        @post = Post.find(params["id"])
-    end
-
     def new
         @post = Post.new
         @post.place_id = params["place_id"]
